6e29ee9c589829eee0a166d3248c9800f3c89950
ben.nagy.web <ben.nagy.web@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-02-06 06:08:12 +0000

    optimised RT, to remove 4 long select operations it was performing in a critical section. wtf was I thinking?

--- src/result_tracker.rb
+++ src/result_tracker.rb
@@ -6,7 +6,7 @@ class ResultTracker
         @prod_clients=0
         @fuzz_clients=0
         @mutex=Mutex.new
-        @results={}
+        @results=Hash.new(0)
         @time_mark=Time.now
         @sent_mark=0
     end
@@ -51,7 +51,8 @@ class ResultTracker
         unless @results[id]==:checked_out
             raise RuntimeError, "RT: The id not checked out yet?"
         end
-        @results[id]=status
+        @results.delete id
+        @results[status]+=1
     ensure
         Thread.critical=false
     end
@@ -74,17 +75,12 @@ class ResultTracker
 
     def spit_results
         Thread.critical=true
-        succeeded=@results.select {|k,v| v==:success}.length
-        hangs=@results.select {|k,v| v==:hang}.length
-        fails=@results.select {|k,v| v==:fail}.length
-        crashes=@results.select {|k,v| v==:crash}.length
-        unknown=@results.select {|k,v| v==:checked_out}.length
         if @sent%100==0
             @sent_mark=@sent
             @time_mark=Time.now
         end
         print "\r"
-        print "Crash: #{crashes}, Fail: #{fails}, Success: #{succeeded}, #{@sent} currently @ #{"%.2f"%((@sent-@sent_mark)/(Time.now-@time_mark).to_f)}/s"
+        print "Crash: #{@results[:crash]}, Fail: #{@results[:fail]}, Success: #{@results[:success]}, #{@sent} currently @ #{"%.2f"%((@sent-@sent_mark)/(Time.now-@time_mark).to_f)}/s"
     ensure
         Thread.critical=false
     end
