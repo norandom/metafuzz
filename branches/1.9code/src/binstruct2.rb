7a2eb31542238bd6843c4c5c24ff387d25a9c436
ben.nagy.web <ben.nagy.web@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-02-25 07:15:32 +0000

    Many changes to fuzzer etc. binstruct2 now renamed binstruct in this branch. Mutations now runs the RollingCorrupt generator for integers as well, to get more coverage. General cleanups in fuzzer, blah blah.

--- branches/1.9code/src/binstruct2.rb
+++ branches/1.9code/src/binstruct2.rb
@@ -1,26 +1,105 @@
+require 'fields'
 require 'objhax'
 
 class Binstruct
 
+    class Bitfield < Binstruct
+    end
+    attr_reader :groups, :endian
+    attr_accessor :fields
     #---------------CONSTRUCTION-------------------
-    #fieldtype builders
     def endian( sym )
         unless sym==:little || sym==:big
             raise RuntimeError, "BinStruct: Construction: Unknown endianness #{sym.to_s}"
         end
+        @endian=sym
         meta_def :endian do sym end
+        meta_def :endianness do sym end
     end
+
+    def bitfield(buf, len, &blk)
+        @bs||=buf.unpack('B*').first
+        if @endian==:little
+            unless len==16||len==32||len==64
+                raise RuntimeError, "Binstruct: Bitfield: Don't know how to endian swap #{len} bits. :("
+            end
+            instr=@bs.slice!(0,len).scan(/.{8}/).reverse.join.reverse
+        else
+            instr=@bs.slice!(0,len)
+        end
+        new=Bitfield.new([instr].pack('B*'), &blk)
+        if @endian==:little
+            new.instance_variable_set :@endian_flip_hack, true
+        end
+        @fields << new
+        new.fields.each {|f| 
+            unless f.is_a? Fields::Field
+                raise RuntimeError, "Binstruct: Construction: Illegal content #{f.class} in bitfield - use only Fields"
+            end
+            @hash_references[f.name]=f
+            meta_def f.name do f.get_value end
+            meta_def (f.name.to_s + '=').to_sym do |val| f.set_value(val) end
+        }
+    end
+
+    def substruct(buf, name, len, klass)
+        @bs||=buf.unpack('B*').first
+        new=klass.new([@bs.slice!(0,len)].pack('B*'))
+        @fields << new
+        @hash_references[name]=new
+        meta_def name do new end
+        # More informative than the NoMethodError they would normally get.
+        meta_def (name.to_s + '=').to_sym do raise NoMethodError, "Binstruct: Illegal call of '=' on a substruct." end
+    end
+
+    #fieldtype builders
+    Fields::Field_Subtypes.each {|fieldname|
+        field_klass=Fields.const_get(fieldname.capitalize.to_s+"Field")
+        define_method fieldname do |*args|
+            buffer, name, len, desc=args
+            @bs||=buffer.unpack('B*').first
+            @fields << thisfield=field_klass.new(@bs.slice!(0,len),name,len,desc,nil,@endian||:big)
+            @hash_references[name.to_sym]=thisfield
+            meta_def name do thisfield.get_value end
+            meta_def (name.to_s + '=').to_sym do |val| thisfield.set_value(val) end
+        end
+    }
 
     def group( groupname, *fieldsyms )
+        @groups[groupname] << fieldsyms
+    end
+
+    class << self
+        attr_reader :init_block
+    end
+    def self.parse( &blk )
+        @init_block=blk
     end
     #------------END CONSTRUCTION------------------
-
-
+    def initialize(*args, &blk)
+        @fields=[]
+        @hash_references={}
+        @endian_flip_hack=false
+        @groups=Hash.new {|h,k| h[k]=[]}
+        args<<"" if args.empty?
+        if block_given?
+            instance_exec(*args, &blk)
+        else
+            instance_exec(*args, &self.class.init_block)
+        end
+        endian :big unless @endian
+        @groups.each {|group, contents|
+            unless contents.flatten.all? {|sym| @hash_references.keys.any? {|othersym| othersym==sym}}
+                raise RuntimeError, "Binstruct: Construction: group #{group} contains invalid field name(s)"
+            end
+        }
+    end
     #----------------INSTANCE----------------------
-    def[]
+    def[]( sym )
         # return an object, specified by symbol. May be a field or a substruct.
         # not designed for bitfields, since they're supposed to be invisible
         # containers.
+        @hash_references[sym]
     end
 
     def each( &blk )
@@ -29,15 +108,43 @@ class Binstruct
         # yield each component, not the container field. The upshot of all this
         # is that the caller needs to be prepared for a Field or a Binstruct in the
         # block. This is the 'shallow' each.
+        @fields.each {|atom|
+            if atom.is_a? Bitfield
+                atom.fields.each {|f| yield f}
+            else
+                yield atom
+            end
+        }
+
     end
-    
-    def deep_each( depth=0, &blk )
-        # yield each field, recursing through substructs.
+
+    def deep_each( &blk )
+        # yield all fields in the structure, entering nested substructs as neccessary
+        @fields.each {|atom|
+            if atom.is_a? Bitfield
+                atom.fields.each {|f| yield f}
+            elsif atom.is_a? Binstruct # but not a Bitfield
+                atom.deep_each &blk
+            else
+                yield atom
+            end
+        }
     end
-    
+
     def to_s
         #pack current struct as a string - for Fields, it will use the bitstring, for
-        #anything else it will use to_s.unpack('B*')
+        #anything else (including Bitfields and Binstructs) it will use to_s.unpack('B*')
+        bits=@fields.inject("") {|str,field| 
+            field.kind_of?(Fields::Field) ?  str << field.bitstring : str << field.to_s.unpack('B*').join
+        } 
+        unless bits.length % 8 == 0
+            puts "Warning, structure not byte aligned, right padding with 0"
+        end
+        bits=bits.reverse if @endian_flip_hack
+        bytearray=bits.scan(/.{1,8}/)
+        bytearray.last << '0'*(8-bytearray.last.length) if bytearray.last.length < 8
+        bytearray=bytearray.reverse if @endian_flip_hack
+        bytearray.map {|byte| "" << byte.to_i(2)}.join
     end
 
     def length
@@ -45,39 +152,34 @@ class Binstruct
     end
 end
 
-class FooStruct < Binstruct
-    def initialize( buf )
-        endian :big
-    end
-end
-=begin
-def WordDgg < Binstruct
-    define {|buffer|
-        endian :big
+
+class WordDgg < Binstruct
+    parse{ |buffer|
+        endian :little
         bitfield(buffer, 16) do |buf|
             unsigned buf, :type, 4, "Object type, 0xF for container"
             unsigned buf, :oid, 12, "Object Identifier"
         end
         unsigned buffer, :content_length, 32, "Content length"
         if self.type==0xF
-            substruct(buffer, :contents, self.content_length) do |instr|
-                WordDgg.new(instr)
-            end
+            substruct(buffer, :contents, self.content_length, WordDgg)
         else
             hexstring buffer, :contents, self.content_length, "Contents"
         end
         if buffer ==""
             self.type=0xF
-            self.oid=0x200
-            self.length=0
-            self.contents=""
+            self.oid=0x222
+            self.content_length=0
         end
     }
 end
-=end
 
 if __FILE__==$0
     puts "Starting tests..."
-    f=FooStruct.new("foo")
-    p f.endian
+    w=WordDgg.new("\x0f\x00\xf0\x00\x00\x00aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    p w.to_s.length
+    p w.type
+    p w.content_length
+    p w[:contents].contents
+    w.deep_each {|atom| puts "#{atom.class}, #{atom.name}"}
 end
--- branches/1.9code/src/word_dggfuzz.rb
+++ branches/1.9code/src/word_dggfuzz.rb
@@ -1,10 +1,6 @@
 require 'rubygems'
-require 'eventmachine'
-require 'em_netstring'
-require 'fuzzprotocol'
 require 'thread'
 require 'fuzzer'
-require 'fib'
 require 'diff/lcs'
 require 'wordstruct'
 require 'ole/storage'
@@ -14,7 +10,7 @@ require 'generators'
 
 class Producer < Generators::NewGen
 
-    START_AT=190000
+    START_AT=0
 
     Template=File.open( File.join(Config["WORK DIR"],"boof.doc"),"rb") {|io| io.read}
 
@@ -47,24 +43,38 @@ class Producer < Generators::NewGen
                 ole=Ole::Storage.open(File.join(Config["WORK DIR"],"boof.doc"),'rb')
                 table_stream=ole.file.read("1Table")
                 ole.close
-                fib.groups[:ol][50..-1].each {|fc,lcb|
-                    next if fib.send(lcb)==0
-                    #get the head, fuzztarget and rest from the table stream
-                    puts "Starting #{fc.to_s}, #{lcb.to_s}, #{fib.send(lcb)}"
-                    ts_head=table_stream[0,fib.send(fc)]
-                    fuzztarget=table_stream[fib.send(fc),fib.send(lcb)]
-                    ts_rest=table_stream[fib.send(fc)+fib.send(lcb)..-1]
-                    raise RuntimeError, "Data Corruption - TS corrupt" unless (ts_head+fuzztarget+ts_rest)==table_stream
-                    raise RuntimeError, "Data Corruption - LCB / fuzztarget length mismatch" unless fib.send(lcb)==fuzztarget.length
-                    #create a new Fuzzer using the fuzztarget
-                    bs=Fuzzer.string_to_binstruct(fuzztarget,16)
-                    raise RuntimeError, "Data Corruption - Binstruct.to_s not fuzztarget" unless bs.to_s == fuzztarget
+                fc=:fcDggInfo
+                lcb=:lcbDggInfo
+                #get the head, fuzztarget and rest from the table stream
+                puts "Starting #{fc.to_s}, #{lcb.to_s}, #{fib.send(lcb)}"
+                ts_head=table_stream[0,fib.send(fc)]
+                fuzztarget=table_stream[fib.send(fc),fib.send(lcb)]
+                ts_rest=table_stream[fib.send(fc)+fib.send(lcb)..-1]
+                raise RuntimeError, "Data Corruption - TS corrupt" unless (ts_head+fuzztarget+ts_rest)==table_stream
+                raise RuntimeError, "Data Corruption - LCB / fuzztarget length mismatch" unless fib.send(lcb)==fuzztarget.length
+                begin
+                    dgg_parsed=[]
+                    fuzzcopy=fuzztarget.clone
+                    while fuzzcopy.length > 0
+                        dgg_parsed << WordStructures::WordDgg.new(fuzzcopy)
+                        if fuzzcopy.length > 0
+                            dgg_parsed << BinStruct.new(fuzzcopy.slice!(0,1)) {|buf| unsigned buf, :foo, 8, "thing"}
+                        end
+                    end
+                rescue
+                    raise RuntimeError, "DggFuzz: #{$!}"
+                end
+                raise RuntimeError, "Data Corruption - Binstruct.to_s not fuzztarget" unless dgg_parsed.map {|s| s.to_s}.join == fuzztarget
+                dgg_parsed.each {|bs|
                     f=Fuzzer.new(bs)
-                    f.preserve_length=true
-                    puts "Expecting #{f.count_tests(1024,false)} tests..." rescue puts $!
+                    #f.preserve_length=true
+                    p f.count_tests(1024,false)
                     f.basic_tests(1024,false) {|fuzz|
                         #head+fuzzed+rest
-                        fuzzed_table=ts_head+fuzz.to_s+ts_rest
+                        fuzzary=dgg_parsed.reject {|obj| obj==bs}.insert(dgg_parsed.index(bs),fuzz)
+                        ts_gunk=fuzzary.map {|bs| bs.to_s}.join
+                        #raise RuntimeError, "DggFuzz: Dgg length mismatch" unless ts_gunk.length==1814
+                        fuzzed_table=ts_head+ts_gunk+ts_rest
                         #write the modified stream
                         Ole::Storage.open(temp_file.path,'rb+') {|ole|
                             ole.file.open("1Table","wb+") {|f| f.write( fuzzed_table )}
@@ -77,13 +87,13 @@ class Producer < Generators::NewGen
                         }
                         newfib=WordStructures::WordFIB.new(raw_fib)
                         #adjust the lcb
-                        newfib.send((lcb.to_s+'=').to_sym, fuzz.length)
+                        newfib.send((lcb.to_s+'=').to_sym, ts_gunk.length)
                         #adjust the offsets for all subsequent structures
-                        extra=fuzz.length-fuzztarget.length
-                        if extra > 0
+                        length_delta=ts_gunk.length-fuzztarget.length
+                        if length_delta != 0
                             fib.groups[:ol].each {|off,len|
                                 if (fib.send(off) > fib.send(fc)) and fib.send(len) > 0
-                                    newfib.send((off.to_s+'=').to_sym, fib.send(off)+extra)
+                                    newfib.send((off.to_s+'=').to_sym, fib.send(off)+length_delta)
                                 end
                             }
                         end
@@ -97,7 +107,6 @@ class Producer < Generators::NewGen
             end
             false
         end
-        START_AT.times do @block.resume end
         super
     end
 
