b69b092faf10fd25e83f759185edd8209eb8e298
ben.nagy.web <ben.nagy.web@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-04-09 08:26:35 +0000

    refactored some stuff as a class

--- trunk/src/fuzzclient2.rb
+++ trunk/src/fuzzclient2.rb
@@ -90,7 +90,7 @@ rescue
 end
 
 
-module FuzzClient
+class FuzzClient < EventMachine::Connection
 
     def initialize(config)
         @config=config
@@ -102,95 +102,6 @@ module FuzzClient
         @template=""
         puts "FuzzClient: Starting up..."
         send_client_startup
-    end
-
-    def prepare_test_file(data, msg_id)
-        begin
-            filename="test-"+msg_id.to_s+".doc"
-            filename=File.join(@config["WORK DIR"],filename)
-            fso=WIN32OLE.new("Scripting.FileSystemObject")
-            path=fso.GetAbsolutePathName(filename) # Sometimes paths with backslashes break things, the FSO always does things right.
-            fso.ole_free
-            File.open(path, "wb+") {|io| io.write data}
-            path
-        rescue
-            raise RuntimeError, "Fuzzclient: Couldn't create test file #{fn} : #{$!}"
-        end
-    end
-
-    def clean_up( fn )
-        10.times do
-            begin
-                FileUtils.rm_f(fn)
-            rescue
-                raise RuntimeError, "Fuzzclient: Failed to delete #{fn} : #{$!}"
-            end
-            return true unless File.exist? fn
-            sleep(0.1)
-        end
-        return false
-    end
-
-    def deliver(data,msg_id)
-        begin
-            status=:error
-            crash_details="" # will only be set to anything if there's a crash
-            this_test_filename=prepare_test_file(data, msg_id)
-            begin
-                5.times do
-                    begin
-                        @word=Connector.new(CONN_OFFICE, 'word')
-                        break
-                    rescue
-                        sleep(1)
-                        next
-                    end
-                end
-                current_pid=@word.pid
-            rescue
-                raise RuntimeError, "Couldn't establish connection to app. #{$!}"
-            end
-            # Attach debugger
-            # -snul - don't load symbols
-            # -c  - initial command
-            # sxe -c "!exploitable -m;g" av - run the MS !exploitable windbg extension
-            # -hd don't use the debug heap
-            # -pb don't request an initial break (not used now, cause we need the break so we can read the initial command)
-            # -x ignore first chance av exceptions
-            # -xi ld ignore module loads
-            debugger=Connector.new(CONN_CDB,"-snul -c \"sxe -c \\\"r;!exploitable -m\\\" av;!load winext\\msec.dll;g\" -hd -x -xi ld -p #{current_pid}")
-            begin
-                @word.deliver this_test_filename
-                status=:success
-                print '.';$stdout.flush
-            rescue
-                # check for crashes
-                sleep(0.1) # This magically seems to fix a race condition.
-                if debugger.crash?
-                    status=:crash
-                    sleep(0.1) while debugger.target_running?
-                    crash_details=debugger.dq_all.join
-                    #File.open(File.join(@config["WORK DIR"],"crash-"+msg_id.to_s+".doc"), "wb+") {|io| io.write(data)}
-                    print '!';$stdout.flush
-                    # If the app has crashed we should kill the debugger, otherwise
-                    # the app won't be killed without -9.
-                    debugger.close
-                else
-                    status=:fail
-                    print '#';$stdout.flush
-                end
-            end
-            # close the debugger and kill the app
-            # This should kill the winword process as well
-            # Clean up the connection object
-            @word.close rescue nil
-            debugger.close 
-            clean_up(this_test_filename) 
-            [status,crash_details]
-        rescue
-            raise RuntimeError, "Delivery: fatal: #{$!}"
-            # ask the server to revert me to my snapshot?
-        end
     end
 
     # Protocol Send functions
@@ -288,9 +199,101 @@ module FuzzClient
     end
 end
 
+class WordFuzzClient < FuzzClient
+      def prepare_test_file(data, msg_id)
+        begin
+            filename="test-"+msg_id.to_s+".doc"
+            filename=File.join(@config["WORK DIR"],filename)
+            fso=WIN32OLE.new("Scripting.FileSystemObject")
+            path=fso.GetAbsolutePathName(filename) # Sometimes paths with backslashes break things, the FSO always does things right.
+            fso.ole_free
+            File.open(path, "wb+") {|io| io.write data}
+            path
+        rescue
+            raise RuntimeError, "Fuzzclient: Couldn't create test file #{fn} : #{$!}"
+        end
+    end
+
+    def clean_up( fn )
+        10.times do
+            begin
+                FileUtils.rm_f(fn)
+            rescue
+                raise RuntimeError, "Fuzzclient: Failed to delete #{fn} : #{$!}"
+            end
+            return true unless File.exist? fn
+            sleep(0.1)
+        end
+        return false
+    end
+
+    def deliver(data,msg_id)
+        begin
+            status=:error
+            crash_details="" # will only be set to anything if there's a crash
+            this_test_filename=prepare_test_file(data, msg_id)
+            begin
+                5.times do
+                    begin
+                        @word=Connector.new(CONN_OFFICE, 'word')
+                        break
+                    rescue
+                        sleep(1)
+                        next
+                    end
+                end
+                current_pid=@word.pid
+            rescue
+                raise RuntimeError, "Couldn't establish connection to app. #{$!}"
+            end
+            # Attach debugger
+            # -snul - don't load symbols
+            # -c  - initial command
+            # sxe -c "!exploitable -m;g" av - run the MS !exploitable windbg extension
+            # -hd don't use the debug heap
+            # -pb don't request an initial break (not used now, cause we need the break so we can read the initial command)
+            # -x ignore first chance av exceptions
+            # -xi ld ignore module loads
+            debugger=Connector.new(CONN_CDB,"-snul -c \"sxe -c \\\"r;!exploitable -m\\\" av;!load winext\\msec.dll;g\" -hd -x -xi ld -p #{current_pid}")
+            begin
+                @word.deliver this_test_filename
+                status=:success
+                print '.';$stdout.flush
+            rescue
+                # check for crashes
+                sleep(0.1) # This magically seems to fix a race condition.
+                if debugger.crash?
+                    status=:crash
+                    sleep(0.1) while debugger.target_running?
+                    crash_details=debugger.dq_all.join
+                    #File.open(File.join(@config["WORK DIR"],"crash-"+msg_id.to_s+".doc"), "wb+") {|io| io.write(data)}
+                    print '!';$stdout.flush
+                    # If the app has crashed we should kill the debugger, otherwise
+                    # the app won't be killed without -9.
+                    debugger.close
+                else
+                    status=:fail
+                    print '#';$stdout.flush
+                end
+            end
+            # close the debugger and kill the app
+            # This should kill the winword process as well
+            # Clean up the connection object
+            @word.close rescue nil
+            debugger.close 
+            clean_up(this_test_filename) 
+            [status,crash_details]
+        rescue
+            raise RuntimeError, "Delivery: fatal: #{$!}"
+            # ask the server to revert me to my snapshot?
+        end
+    end
+
+end
+
 EventMachine::run {
     system("start ruby wordslayer.rb")
     system("start ruby dk.rb")
-    EventMachine::connect(config["SERVER IP"],config["SERVER PORT"], FuzzClient, config)
+    EventMachine::connect(config["SERVER IP"],config["SERVER PORT"], WordFuzzClient, config)
 }
 puts "Event loop stopped. Shutting down."
