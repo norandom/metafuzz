aba62cd28a0d2c9cf6aaf143654ae5e6014b0102
ben.nagy.web <ben.nagy.web@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-05-25 03:26:52 +0000

    Cosmetic output changes

--- branches/1.9code/src/summarize_results.rb
+++ branches/1.9code/src/summarize_results.rb
@@ -3,22 +3,24 @@ path=ARGV[0]
 # get all detail files in the path
 pattern=File.join(path, "detail*.txt")
 results=Hash.new {|hsh, k| hsh[k]=[0,""]}
+
+def dump(results)
+	results.sort.each {|k,v|
+		puts "--- #{k} (count: #{v[0]}) ---"
+		puts v[1]
+	}
+end
 Dir.glob(pattern, File::FNM_DOTMATCH).each {|fn|
-    contents=File.open(fn, "rb") {|ios| ios.read}
-    if match=contents.match(/Hash=(.*)\)/)
-        bucket=match[1]
-        results[bucket][0]+=1
-        next if results[bucket][0] > 1
-        title=contents.scan(/^DESCRIPTION.*$/).join
-        classification=contents.scan(/^CLASSIFICATION.*$/).join
-        instructions=contents.scan(/^BASIC_BLOCK_INSTRUCTION:.*$/).join("\n")
-        results[bucket][1]=[title, instructions, classification].join("\n")
-    end
+	contents=File.open(fn, "rb") {|ios| ios.read}
+	if match=contents.match(/Hash=(.*)\)/)
+		bucket=match[1]
+		results[bucket][0]+=1
+		next if results[bucket][0] > 1
+		classification=contents.scan(/^CLASSIFICATION.*$/).join
+		instructions=contents.scan(/^BASIC_BLOCK_INSTRUCTION:.*$/).join("\n")
+		title=contents.scan(/^(BUG_TITLE.*) \(/).join
+		file=fn
+		results[bucket][1]=[title, instructions, classification, file].join("\n")
+	end
 }
-
-results.sort.each {|k,v|
-    puts "--- #{k} (count: #{v[0]}) ---"
-    puts v[1]
-}
-
-
+dump results
