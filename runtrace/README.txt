3027694563439742bc5c7f0f3cb36e94761c256f
thegrugq <thegrugq@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-08-25 03:30:41 +0000

    adding README.txt

---
+++ runtrace/README.txt
@@ -0,0 +1,23 @@
+
+
+REQUIREMENTS:
+------------
+
+step 1: Python runtime environment
+    Python
+        http://python.org/ftp/python/2.6.2/python-2.6.2.msi
+    easy_install
+        http://pypi.python.org/packages/2.5/s/setuptools/setuptools-0.6c9.win32-py2.5.exe
+
+step 2: Python packages
+    SQLAlchemy
+        $ easy_install sqlalchemy
+    psycopg2
+        download -> http://initd.org/pub/software/psycopg/psycopg2-2.0.12.tar.gz
+        XXX - best to just package this, it'll never work otherwise.
+
+INSTALLATION
+------------
+
+The runtracer must be installed into the C:\runtracer\ directory. This can be
+done by moving the unpacked source code into the correct directory.
--- runtrace/tracerdb.py
+++ runtrace/tracerdb.py
@@ -2,40 +2,89 @@
 #
 # (c) 2009, the grugq <the.grugq@gmail.com>
 
+from sqlalchemy import (Column, Integer, String, ForeignKey, create_engine)
+from sqlalchemy.orm import (relation, backref, sessionmaker)
+from sqlalchemy.ext.declarative import (declarative_base,)
 
-from sqlalchemy import (Column, Integer, String)
+from contextlib import contextmanager
 
+
+Base = declarative_base()
 
 class Module(Base):
     __tablename__ = "modules"
 
-    id  = Column(Integer, primary=True)
+    id  = Column(Integer, primary_key=True)
     name= Column(String, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Module(%d) %s>" % (self.id, self.name)
 
 class Function(Base):
     __tablename__ = "functions"
 
-    id  = Column(Integer, primary=True)
+    id  = Column(Integer, primary_key=True)
     name= Column(String, nullable=False)
+    address     = Column(Integer, nullable=False)
     module_id   = Column(Integer, ForeignKey("modules.id"))
 
-class StackTrace(Base):
-    __tablename__ = "stacktraces"
+    module  = relation(Module, backref=backref("functions", order_by=address))
 
-    id  = Column(Integer, primary=True)
-    filename = Column(String, nullable=False)
+    def __init__(self, address, name, module):
+        self.address= address
+        self.name   = name
+        self.module = module
 
+    def __repr__(self):
+        return "<Function 0x%08x %s [%s]>" % (self.address, self.name, self.module.name)
 
 class Frame(Base):
     __tablename__ = "frames"
 
-    id  = Column(Integer, primary=True)
+    id  = Column(Integer, primary_key=True)
     sequence    = Column(Integer, nullable=False)
     function_id = Column(Integer, ForeignKey("functions.id"))
     trace_id    = Column(Integer, ForeignKey("stacktraces.id"))
 
-def create_stack_trace(fname, frames):
-    trace = StackTrace(fname)
+    function    = relation(Function)
 
-    for sequence, func_id in enumerate(frames):
-        trace.append(Frame(func_id, sequence=sequence))
+    def __init__(self, sequence, function, trace):
+        self.sequence = sequence
+        self.function = function
+        self.stacktrace = trace
+
+class StackTrace(Base):
+    __tablename__ = "stacktraces"
+
+    id  = Column(Integer, primary_key=True)
+    filename = Column(String, nullable=False)
+
+    frames  = relation(Frame, order_by=Frame.sequence, backref="stacktrace")
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __repr__(self):
+        return "<StackTrace: %s [frames: %d]>" % (self.filename,)
+
+
+class TraceDB(object):
+    def __init__(self, uri):
+        self.engine = create_engine(uri)
+        self.create_session = sessionmaker(bind=self.engine)
+
+        Base.metadata.create_all(self.engine)
+
+    @contextmanager
+    def session(self):
+        session = self.create_session()
+        try:
+            yield session
+        except:
+            session.rollback()
+            raise
+        else:
+            session.commit()
