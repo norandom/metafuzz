aa86d59f7786c6f4eedb5432ec0bcdd6c5d13c6b
thegrugq <thegrugq@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-08-27 20:18:57 +0000

    revamped export_functions, skeleton TracerDB. Requires testing.

--- runtrace/tracerdb.py
+++ runtrace/tracerdb.py
@@ -1,13 +1,30 @@
-#!C:\python25\python.exe
+#!/usr/bin/env python
 #
 # (c) 2009, the grugq <the.grugq@gmail.com>
 
-from sqlalchemy import (Column, Integer, String, ForeignKey, create_engine)
-from sqlalchemy.orm import (relation, backref, sessionmaker)
-from sqlalchemy.ext.declarative import (declarative_base,)
-
 from contextlib import contextmanager
+from sqlalchemy import (Column, String, Integer, ForeignKey)
+from sqlalchemy.ext.declarative import declarative_base
 
+import os
+import sqlite3
+
+DB_PATH="C:\\runtracer\\modules.sqlite"
+
+# SCHEMA
+def create_db(path="C:\\runtracer\\modules.sqlite"):
+    module_table = """CREATE TABLE modules IF NOT EXISTS (
+    id  INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    path TEXT NOT NULL,
+    md5  TEXT NOT NULL
+    );"""
+    function_table = """CREATE TABLE functions IF NOT EXISTS (
+    id  INTEGER PRIMARY KEY,
+    address INTEGER NOT NULL,
+    name    TEXT NOT NULL,
+    module_id  INTEGER REFERENCES modules (id),
+    )"""
 
 Base = declarative_base()
 
@@ -15,76 +32,58 @@ class Module(Base):
     __tablename__ = "modules"
 
     id  = Column(Integer, primary_key=True)
-    name= Column(String, nullable=False)
+    name    = Column(String, nullable=False)
+    path    = Column(String, nullable=False)
+    md5sum  = Column(String, nullable=False)
 
-    def __init__(self, name):
+    def __init__(self, name, path, md5sum):
         self.name = name
-
+        self.path = path
+        self.md5sum = md5sum
     def __repr__(self):
-        return "<Module(%d) %s>" % (self.id, self.name)
+        return "<Module(%s) %s [%s]>" % (self.name, self.path, self.md5sum)
 
 class Function(Base):
     __tablename__ = "functions"
 
     id  = Column(Integer, primary_key=True)
-    name= Column(String, nullable=False)
-    address     = Column(Integer, nullable=False)
-    module_id   = Column(Integer, ForeignKey("modules.id"))
+    address = Column(Integer, nullable=False)
+    name    = Column(String, nullable=False)
+    module_id=Column(Integer, ForeignKey("modules.id"))
 
-    module  = relation(Module, backref=backref("functions", order_by=address))
+    module  = reference(Module, backref=backref('functions', order_by=id))
 
     def __init__(self, address, name, module):
-        self.address= address
-        self.name   = name
+        self.address = address
+        self.name = name
         self.module = module
 
     def __repr__(self):
-        return "<Function 0x%08x %s [%s]>" % (self.address, self.name, self.module.name)
+        return "<Function %08x %s::%s>" % (self.address, self.module.name,
+                                           self.name)
 
-class Frame(Base):
-    __tablename__ = "frames"
+class ModuleList(object):
+    def __init__(self, db):
+        self.db = db
+    def query(self, **kwargs):
+        return self.db.session.query(Module).filter_by(**kwargs)
+    def by_path(self, path):
+        return self.query(path=path)
+    def by_name(self, name):
+        return self.query(name=name)
+    def by_md5sum(self, md5sum):
+        return self.query(md5sum=md5sum)
+    def __getitem__(self, name):
+        return self.from_name(name)
+    def __iter__(self):
+        return self.db.session.query(Module).all()
+    def keys(self):
+        return self.db.session.query(Module.name).all()
 
-    id  = Column(Integer, primary_key=True)
-    sequence    = Column(Integer, nullable=False)
-    function_id = Column(Integer, ForeignKey("functions.id"))
-    trace_id    = Column(Integer, ForeignKey("stacktraces.id"))
-
-    function    = relation(Function)
-
-    def __init__(self, sequence, function, trace):
-        self.sequence = sequence
-        self.function = function
-        self.stacktrace = trace
-
-class StackTrace(Base):
-    __tablename__ = "stacktraces"
-
-    id  = Column(Integer, primary_key=True)
-    filename = Column(String, nullable=False)
-
-    frames  = relation(Frame, order_by=Frame.sequence, backref="stacktrace")
-
-    def __init__(self, filename):
-        self.filename = filename
-
-    def __repr__(self):
-        return "<StackTrace: %s [frames: %d]>" % (self.filename,)
-
-
-class TraceDB(object):
+class TracerDB(object):
     def __init__(self, uri):
         self.engine = create_engine(uri)
-        self.create_session = sessionmaker(bind=self.engine)
-
+        self.session = Session()
         Base.metadata.create_all(self.engine)
 
-    @contextmanager
-    def session(self):
-        session = self.create_session()
-        try:
-            yield session
-        except:
-            session.rollback()
-            raise
-        else:
-            session.commit()
+        self.modules = ModuleList(self)
