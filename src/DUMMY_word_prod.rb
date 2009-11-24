61943c8d96e863fca935f71acb34756da6eb0d7e
ben.nagy.web@gmail.com <ben.nagy.web@gmail.com@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-11-24 07:20:08 +0000

    dummy producer, just sends the template

---
+++ src/DUMMY_word_prod.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'fuzzer'
+require 'wordstruct'
+require 'ole/storage'
+
+class Producer < Generators::NewGen
+
+    START_AT=0
+
+    Template=File.open( File.expand_path("~/fuzzserver/boof.doc"),"rb") {|io| io.read}
+
+    def seen?( str )
+        hsh=Digest::MD5.hexdigest(str)
+        seen=@duplicate_check[hsh]
+        @duplicate_check[hsh]=true
+        @duplicate_check.shift if @duplicate_check.size > SEEN_LIMIT
+        seen
+    end
+
+    def initialize
+        @duplicate_check=Hash.new(false)
+        @block=Fiber.new do
+            begin
+                Fiber.yield Template
+            rescue
+                puts "Production failed: #{$!}";$stdout.flush
+                exit
+            end
+            false
+        end
+        super
+    end
+end
