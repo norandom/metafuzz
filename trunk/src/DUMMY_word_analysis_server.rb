01b759d6f60be8642e84f28a3d8355e6717307cc
ben.nagy.web <ben.nagy.web@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-11-30 03:43:16 +0000

    random

--- trunk/src/DUMMY_word_analysis_server.rb
+++ trunk/src/DUMMY_word_analysis_server.rb
@@ -42,7 +42,7 @@ EM.set_max_timers(50000000)
 EventMachine::run {
     # Anything not set up here gets the default value.
     AnalysisServer.setup(
-        'debug'=>false, 
+        'debug'=>true, 
         'server_ip'=>'192.168.242.101',
         'poll_interval'=>50
 )
