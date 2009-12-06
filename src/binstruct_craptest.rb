bb8ab3eb1812e1ba4f11638e1d44b738483d7e97
ben.nagy.web@gmail.com <ben.nagy.web@gmail.com@9f69444c-ac94-11dd-8fd9-859366ca643a>
2009-12-06 02:45:05 +0000

    Using pack instead of map{ |e| e.to_i(2).chr}, should be faster

---
+++ src/binstruct_craptest.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'zlib'
+require 'binstruct'
+require 'wordstruct'
+require 'ole/storage'
+Template=File.open("dgg.doc","rb") {|io| io.read}
+            begin
+                unmodified_file=StringIO.new(Template.clone)
+                header=unmodified_file.read(512)
+                raw_fib=unmodified_file.read(1472)
+                rest=unmodified_file.read
+                raise RuntimeError, "Data Corruption" unless header+raw_fib+rest == Template
+                fib=WordStructures::WordFIB.new(raw_fib.clone)
+                raise RuntimeError, "Data Corruption - fib.to_s not raw_fib" unless fib.to_s == raw_fib
+                # Open the file, get a copy of the table stream
+                unmodified_file.rewind
+                ole=Ole::Storage.open(unmodified_file)
+                # get the correct table stream 1Table or 0Table
+                table_stream=ole.file.read(fib.fWhichTblStm.to_s+"Table")
+                ole.close
+                fc=:fcDggInfo
+                lcb=:lcbDggInfo
+                #get the head, fuzztarget and rest from the table stream
+                puts "Starting #{fc.to_s}, #{lcb.to_s}, #{fib.send(lcb)}"
+                ts_head=table_stream[0,fib.send(fc)]
+                fuzztarget=table_stream[fib.send(fc),fib.send(lcb)]
+                ts_rest=table_stream[fib.send(fc)+fib.send(lcb)..-1]
+                raise RuntimeError, "Data Corruption - TS corrupt" unless (ts_head+fuzztarget+ts_rest)==table_stream
+                raise RuntimeError, "Data Corruption - LCB / fuzztarget length mismatch" unless fib.send(lcb)==fuzztarget.length
+                begin
+                    dgg_parsed=[]
+                    fuzzcopy=fuzztarget.clone
+                    while fuzzcopy.length > 0
+                        dgg_parsed << WordStructures::WordDgg.new(fuzzcopy)
+                        if fuzzcopy.length > 0
+                            dgg_parsed << Binstruct.new(fuzzcopy.slice!(0,1)) {|buf| unsigned buf, :foo, 8, "thing"}
+                        end
+                    end
+                rescue
+                    raise RuntimeError, "DggFuzz: #{$!}"
+                end
+                raise RuntimeError, "Data Corruption - Binstruct.to_s not fuzztarget" unless dgg_parsed.map {|s| s.to_s}.join == fuzztarget
+                p dgg_parsed
+            rescue Exception => e
+                puts "Production failed: #{$!}";$stdout.flush
+                puts e.backtrace
+                exit
+            end
