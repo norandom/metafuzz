fcf192275adf189b7a93a0b4917dab1b3c7ef292
ben.nagy.web@gmail.com <ben.nagy.web@gmail.com@9f69444c-ac94-11dd-8fd9-859366ca643a>
2010-05-23 12:33:23 +0000

    Some changes to make dumy testing easier

---
+++ src/word_fuzzserver/DUMMY_prodclient.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require File.dirname(__FILE__) + '/../core/fuzzer'
+require 'trollop'
+
+OPTS = Trollop::options do 
+    opt :size, "Size of tests in KB", :type => :integer, :required => true
+end
+
+# Skeleton for a Producer generator - this is the bit that actually does the case
+# generation.
+#
+# ---
+# This file is part of the Metafuzz fuzzing framework.
+# Author: Ben Nagy
+# Copyright: Copyright (c) Ben Nagy, 2006-2009.
+# License: All components of this framework are licensed under the Common Public License 1.0. 
+# http://www.opensource.org/licenses/cpl1.0.txt
+class Producer < Generators::NewGen
+
+    Template=("A" * (OPTS[:size] * 1024))
+
+    def initialize
+        @duplicate_check=Hash.new(false)
+        @block=Fiber.new do
+            loop do
+            # This will just send the template over and over. 
+            # To actually fuzz, make changes and yield at each step.
+            Fiber.yield Template
+            end
+            false
+        end
+        super
+    end
+end
