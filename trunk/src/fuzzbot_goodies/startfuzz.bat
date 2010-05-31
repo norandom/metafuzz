869944c0023961a41f5f5e5965b7e4a183348578
ben.nagy.web@gmail.com <ben.nagy.web@gmail.com@9f69444c-ac94-11dd-8fd9-859366ca643a>
2010-05-31 07:42:57 +0000

    try 90 secs :(

--- trunk/src/fuzzbot_goodies/startfuzz.bat
+++ trunk/src/fuzzbot_goodies/startfuzz.bat
@@ -1,6 +1,6 @@
 =====================
 rmdir /s /q c:\fuzzbot_code
-ping -c 30 192.168.122.1
+ping -c 90 192.168.122.1
 xcopy /d /y /s \\192.168.122.1\ramdisk\fuzzbot_code c:\fuzzbot_code\
 copy /y c:\fuzzbot_code\startfuzz.bat c:\AUTOEXEC.BAT
 c:\fuzzbot_code\compname /c BUGMINER-?8
