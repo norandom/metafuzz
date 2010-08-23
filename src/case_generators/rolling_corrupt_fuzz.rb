0fd76f6530ced3104e3ae91c0b1c265ac8502a1c
ben.nagy.web <ben.nagy.web@9f69444c-ac94-11dd-8fd9-859366ca643a>
2010-08-23 05:32:02 +0000

    Upgrade rolling_corrupt to harness 3.5 (tags etc)

--- src/case_generators/rolling_corrupt_fuzz.rb
+++ src/case_generators/rolling_corrupt_fuzz.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require File.dirname(__FILE__) + '/../core/fuzzer_new'
+require 'trollop'
+
 
 # This is a dumb fuzzer which doesn't even unpack the OLE file
 # It's the simplest use of my fuzzer class / algorithms
@@ -12,8 +14,17 @@ require File.dirname(__FILE__) + '/../core/fuzzer_new'
 # http://www.opensource.org/licenses/cpl1.0.txt
 class Producer < Generators::NewGen
 
-    def initialize( template_fname, granularity=128)
-        @template=File.open( template_fname ,"rb") {|io| io.read}
+    def initialize( args, prodclient_klass )
+        @opts=Trollop::options( args ) do
+            opt :granularity, "Granularity - number of fields to cut file into", :type=>:integer, :default=>128
+            opt :template, "Template filename", :type=>:string, :required=>true
+        end
+        @template=File.open( @opts[:template] ,"rb") {|io| io.read}
+        our_tag=""
+        our_tag << "ROLLINGCORRUPT_GRANULARITY:#{@opts[:granularity]}\n"
+        our_tag << "ROLLINGCORRUPT_TEMPLATE:#{@opts[:template]}\n"
+        our_tag << "ROLLINGCORRUPT_TEMPLATE_MD5:#{Digest::MD5.hexdigest(@template)}\n"
+        prodclient_klass.base_tag=prodclient_klass.base_tag << our_tag
         @duplicate_check=Hash.new(false)
         @fuzztarget=Fuzzer.string_to_binstruct( @template.clone, granularity, endian=:little )
         @block=Fiber.new do
